0a94bfd3a30b05dc07615f6c25f9e9d251cc36ad
黄溪舟 <xizhouh@gmail.com>
2017-10-16 12:34:30 -0500

    init

--- autoBuild.sh
+++ autoBuild.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
-git pull origin master
-npm install
+SITE_PATH='/var/www/html/autoBuild'
+USER='root'
+
+cd $SITE_PATH
+git reset --hard origin/master
+git clean -f
+git pull
+npm i
 npm start
+
+chown -R $USER $SITE_PATH
