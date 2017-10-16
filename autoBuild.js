555db1b5ce6af7c4a39af6f57c6504b4f5c23fb3
黄溪舟 <xizhouh@gmail.com>
2017-10-16 16:46:52 -0500

    更新autoBuild.js

--- autoBuild.js
+++ autoBuild.js
@@ -1,27 +1,30 @@
-var http = require('http');
-var spawn = require('child_process').spawn;
-var createHandler = require('github-webhook-handler');
-var handler = createHandler({ path: '/pushCode', secret: '123' });
+var http = require('http')
+var spawn = require('child_process').spawn
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/pushCode', secret: '123' })
 http.createServer(function (req, res) {
   handler(req, res, function (err) {
     res.statusCode = 404;
-    res.end('no such location');
+    res.end('no such location')
   })
-}).listen(7777);
+}).listen(7777)
+
 handler.on('error', function (err) {
   console.error('Error:', err.message)
-});
+})
+
 handler.on('push', function (event) {
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
-    event.payload.ref);
-  rumCommand('sh', ['./autoBuild.sh'], function( txt ){
-    console.log(txt);
-  });
-});
-function rumCommand( cmd, args, callback ){
-    var child = spawn( cmd, args );
-    var resp = '';
-    child.stdout.on('data', function( buffer ){ resp += buffer.toString(); });
-    child.stdout.on('end', function(){ callback( resp ) });
+    event.payload.ref)
+  rumCommand('sh', ['./autoBuild.sh'], function( txt ) {
+    console.log(txt)
+  })
+})
+
+function rumCommand( cmd, args, callback ) {
+    var child = spawn( cmd, args )
+    var response = ''
+    child.stdout.on('data', function( buffer ){ response += buffer.toString(); })
+    child.stdout.on('end', function(){ callback( response ) })
 }
